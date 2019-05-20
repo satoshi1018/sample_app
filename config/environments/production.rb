42bc37ac4b2a15bbe45a9ebef17bb96dad66095f
satoshi <satoshi19921117505@gmail.com>
2019-05-20 18:35:23 +0900

    send_email 修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app133373065@heroku.com'],
-    :password       => ENV['b1fgspgf8356'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
